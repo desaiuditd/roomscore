1448521d8aa627a4586319849bc7292e3a89ba92
Udit Desai <desaiuditd@gmail.com>
2016-07-07 15:16:00 -0700

    adds mobile config

---
+++ mobile-config.js
@@ -0,0 +1,33 @@
+/**
+ * Created by udit on 07/07/16.
+ */
+
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'tech.roomscore.app',
+  name: 'RoomScore',
+  description: 'Improve Your Score',
+  author: 'RoomScore Team',
+  email: 'desaiuditd@gmail.com',
+  website: 'https://roomscore.tech',
+});
+
+// Set up resources such as icons and launch screens.
+// App.icons({
+//   iphone_2x: 'public/imgs/logo.png', // (120x120)
+//   iphone_3x: 'public/imgs/logo.png', // (180x180)
+//   ipad: 'public/imgs/logo.png', // (76x76)
+//   ipad_2x: 'public/imgs/logo.png', // (152x152)
+//   ipad_pro: 'public/imgs/logo.png', // (167x167)
+//   ios_settings: 'public/imgs/logo.png', // (29x29)
+//   ios_settings_2x: 'public/imgs/logo.png', // (58x58)
+//   ios_settings_3x: 'public/imgs/logo.png', // (87x87)
+//   ios_spotlight: 'public/imgs/logo.png', // (40x40)
+//   ios_spotlight_2x: 'public/imgs/logo.png', // (80x80)
+//   android_mdpi: 'public/imgs/logo.png', // (48x48)
+//   android_hdpi: 'public/imgs/logo.png', // (72x72)
+//   android_xhdpi: 'public/imgs/logo.png', // (96x96)
+//   android_xxhdpi: 'public/imgs/logo.png', // (144x144)
+//   android_xxxhdpi: 'public/imgs/logo.png', // (192x192)
+// });
